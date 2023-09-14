3089c5b3431dd1a63a1d918561a7ee4b891e1f8c
Anirudh Pandey <78467847+anirudh716@users.noreply.github.com>
2023-09-14 19:40:31 +0530

    Time: 0 ms (100%), Space: 6.3 MB (17.61%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,34 +1,14 @@
 class Solution {
 public:
     int reverse(int x) {
-        if (x == 0) return 0;
+        long long reversed = 0;
+        while(x != 0){
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
 
-        // Convert the integer to a string
-        string numStr = to_string(x);
-    
-        // Initialize pointers
-        int i = (x < 0) ? 1 : 0;
-        int j = numStr.length() - 1;
-    
-        // Reverse the string
-        while (i < j) {
-            swap(numStr[i], numStr[j]);
-            i++;
-            j--;
-        }
-    
-        // Remove leading zeros
-        while (numStr[i] == '0') {
-            i++;
-        }
-    
-        // Check for overflow
-        long long result = stoll(numStr); // Convert back to long long to check for     overflow
-        if (result > INT_MAX || result < INT_MIN) {
-            return 0; // Overflow
-        }
-    
-        return static_cast<int>(result); // Convert back to int
+        if(reversed < INT_MIN || reversed > INT_MAX)
+            return 0;
+        return reversed;
     }
-
 };
