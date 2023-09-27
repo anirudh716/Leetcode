ad59076329fce0a80df98b78a17ff5546e58a7b8
Anirudh Pandey <78467847+anirudh716@users.noreply.github.com>
2023-09-28 00:11:52 +0530

    Time: 0 ms (100%), Space: 6.6 MB (7.16%) - LeetHub

---
+++ 0916-decoded-string-at-index/0916-decoded-string-at-index.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        long long decodedLength = 0;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            if (isdigit(s[i])) {
+                decodedLength *= s[i] - '0';
+            } else {
+                decodedLength++;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            k %= decodedLength;
+            if (k == 0 && isalpha(s[i])) {
+                return string(1, s[i]);
+            }
+
+            if (isdigit(s[i])) {
+                decodedLength /= s[i] - '0';
+            } else {
+                decodedLength--;
+            }
+        }
+
+        return "";
+    }
+};
